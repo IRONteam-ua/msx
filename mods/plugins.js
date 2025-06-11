313340161d4a622562df2217255d9e83feb889a9
IRON <35447958+IRONteam-ua@users.noreply.github.com>
2025-06-11 13:08:27 +0300

    Update plugins.js

--- mods/plugins.js
+++ mods/plugins.js
@@ -7,10 +7,6 @@
             ru: "Дополнительные плагины",
         }
     });
-    Lampa.Plugin.register('iron_store', {
-            name: "IRON's Plugins",
-            author: "@IRONteam"
-        });
     function addStore() {
         if (Lampa.Settings.main && !Lampa.Settings.main().render().find('[data-component="iron_store"]').length) {
             var field = "<div class=\"settings-folder selector\" data-component=\"iron_store\" data-static=\"true\">\n\
